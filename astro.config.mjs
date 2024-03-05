04d133e98bb863bfff36901d5fd611d8a40e854c
Hakkodevelopment <legosnake123@gmail.com>
2024-03-04 23:39:06 -0700

    remove netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import NetlifyCMS from "astro-netlify-cms";
 import dcapConfig from "./decap.config.mjs";
 import astropodConfig from "./.astropod/astropod.config.json";
 import robotsTxt from "astro-robots-txt";
@@ -30,9 +29,6 @@ export default defineConfig({
       serviceEntryPoint: "@astrojs/image/sharp",
       cacheDir: "./.cache/image",
       logLevel: "debug",
-    }),
-    NetlifyCMS({
-      config: dcapConfig(),
     }),
   ],
 });
